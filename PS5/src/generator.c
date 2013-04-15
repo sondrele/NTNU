9d37d4ce2a85e7365d34ac36b9a6d840e23b25ba
Sondre Lefsaker <leffsa@gmail.com>
2013-04-15 21:47:51 +0200

    Uploading...

--- PS5/src/generator.c
+++ PS5/src/generator.c
@@ -166,8 +166,8 @@ void generate ( FILE *stream, node_t *root )
 			RECUR ();
 			// Added a NEWLINE-string in symtab.c, this is printed
 			// whenever a PRINT_LIST occurs
-			instruction_add ( PUSH, STRDUP( "$.NEWLINE" ), NULL, 0, 0 );
-			instruction_add ( SYSCALL, STRDUP( "printf" ), NULL, 0, 0 );
+			instruction_add ( PUSH, STRDUP( '\n' ), NULL, 0, 0 );
+			instruction_add ( SYSCALL, STRDUP( "putc" ), NULL, 0, 0 );
 			instruction_add ( ADD, STRDUP( "$4" ), esp, 0, 0 );
 			break;
 		}
