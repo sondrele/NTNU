cc8e03981a7636fc1277c749db25067f3fd4f8ff
Sondre Lefsaker <sondrele@stud.ntnu.no>
2013-10-01 10:46:20 +0200

    added ps3

---
+++ TDT4200/PS3/gemm/fast.c
@@ -0,0 +1,24 @@
+#include <complex.h>
+#include <xmmintrin.h>
+#include <pmmintrin.h>
+
+//Currently just a copy of naive...
+
+void gemm(complex float* A,
+        complex float* B,
+        complex float* C,
+        int m,
+        int n,
+        int k,
+        complex float alpha,
+        complex float beta){
+
+    for(int x = 0; x < n; x++){
+        for(int y = 0; y < m; y++){
+            C[y*n + x] *= beta;
+            for(int z = 0; z < k; z++){
+                C[y*n + x] += alpha*A[y*k+z]*B[z*n + x];
+            }
+        }
+    }
+}
