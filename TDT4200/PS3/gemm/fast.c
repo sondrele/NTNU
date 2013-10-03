d58fd8536b7141da9fec4292a0b6a1fe10d06ff9
Sondre Lefsaker <sondrele@stud.ntnu.no>
2013-10-03 10:02:17 +0200

    better simd

--- TDT4200/PS3/gemm/fast.c
+++ TDT4200/PS3/gemm/fast.c
@@ -2,7 +2,16 @@
 #include <xmmintrin.h>
 #include <pmmintrin.h>
 
-void simd_complex_mult(complex float *, complex float *, complex float *);
+void simd_complex_mult_short(complex float *a, complex float *b, complex float *r);
+void simd_complex_mult(complex float *, complex float *, complex float *, complex float *);
+
+void transpose(complex float *M, int k, int n, complex float *R) {
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < n; j++) {
+            R[i * n + j] = M[j * k + i];
+        }
+    }
+}
 
 void gemm(complex float *A,
         complex float *B,
@@ -22,32 +31,35 @@ void gemm(complex float *A,
     betas[0] = beta;
     betas[1] = beta;
 
-    complex float *a_res = malloc(sizeof(complex float));
-    complex float *b_res = malloc(sizeof(complex float));
-    complex float *c_res = malloc(sizeof(complex float));
+    complex float *a_res = malloc(sizeof(complex float) * 2);
+    complex float *b_res = malloc(sizeof(complex float) * 2);
 
-    for (int x = 0; x < n; x++) {
+    complex float *T = malloc(sizeof(complex float) * n * k);
+    transpose(B, k, n, T);
+
+    for (int x = 0; x < n; x += 2) {
         for (int y = 0; y < m; y++) {
-            C[y*n + x] *= beta;
-            // simd_complex_mult(betas, (C + y*n + x), c_res);
-            // C[y*n + x] = c_res[0];
-            // C[y*n + x + 1] = c_res[1];
+            simd_complex_mult_short((C + y*n + x), betas, (C + y*n + x));
 
             for (int z = 0; z < k; z++) {
-                simd_complex_mult(alphas, (A + y*k + z), a_res);
-                simd_complex_mult(a_res, (B + z*n + x), b_res);
+                // simd_complex_mult(alphas, (A + y*k + z), (T + x*n + z * 2), b_res);
+                // C[y*n + x] += b_res[0];
+                // C[y*n + x + 1] += b_res[1];
+
+                simd_complex_mult(alphas, (A + y*k + z), (B + z*n + x), b_res);
                 C[y*n + x] += b_res[0];
-                // TODO: z += 2, indekser riktig og putt inn _to_ riktige elementer fra B! - altså ikke x  + width
+                simd_complex_mult(alphas, (A + y*k + z), (B + z*n + x + 1), b_res);
+                C[y*n + x + 1] += b_res[0];
             }
         }
     }
 
     free(a_res);
     free(b_res);
-    free(c_res);
+    free(T);
 }
 
-void simd_complex_mult(complex float *a,
+void simd_complex_mult_short(complex float *a,
                        complex float *b,
                        complex float *r) {
     __m128 a_reg,
@@ -65,6 +77,38 @@ void simd_complex_mult(complex float *a,
     a_reg = _mm_shuffle_ps(a_reg, a_reg, 0xb1);
     t_reg1 = _mm_movehdup_ps(b_reg);
     t_reg1 = t_reg1 * a_reg;
+
+    r_reg = _mm_addsub_ps(t_reg2, t_reg1);
+    _mm_storeu_ps((float *) r, r_reg);
+}
+
+void simd_complex_mult(complex float *a, complex float *b, complex float *c, complex float *r) {
+    __m128 a_reg,
+           b_reg,
+           c_reg,
+           t_reg1,
+           t_reg2,
+           r_reg;
+
+    a_reg = _mm_loadu_ps((float *) b);
+    b_reg = _mm_loadu_ps((float *) a); 
+    c_reg = _mm_loadu_ps((float *) c);
+
+    t_reg1 = _mm_moveldup_ps(b_reg);
+    t_reg2 = t_reg1 * a_reg;
+
+    a_reg = _mm_shuffle_ps(a_reg, a_reg, 0xb1);
+    t_reg1 = _mm_movehdup_ps(b_reg);
+    t_reg1 = t_reg1 * a_reg;
+
+    r_reg = _mm_addsub_ps(t_reg2, t_reg1);
+
+    t_reg1 = _mm_moveldup_ps(r_reg);
+    t_reg2 = t_reg1 * c_reg;
+
+    c_reg = _mm_shuffle_ps(c_reg, c_reg, 0xb1);
+    t_reg1 = _mm_movehdup_ps(r_reg);
+    t_reg1 = t_reg1 * c_reg;
 
     r_reg = _mm_addsub_ps(t_reg2, t_reg1);
     _mm_storeu_ps((float *) r, r_reg);
